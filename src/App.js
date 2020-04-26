da38051c888b9e7e18097e92c4be8223d76efd07
Stephanie Fashagba <stephanie.fashagba@thoughtworks.com>
2020-04-26 16:26:32 +0100

    "first commit"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { TimelineLite } from "gsap";
+import { letterArray } from "./constants/images";
+import "./App.css";
 
-function App() {
+const App = () => {
+  let tween = new TimelineLite();
+  let imageElements = [];
+
+  useEffect(() => {
+    imageElements.map((letter) => {
+      return tween.to(letter, randomPositionGenerator());
+    });
+
+    tween
+      .to(imageElements, 3, { autoAlpha: 1 })
+      .to(imageElements, 1, { x: 0, y: 0, z: 0, autoAlpha: 1 });
+  }, [imageElements, tween]);
+
+  const renderLetters = () => {
+    return letterArray.map((imgUrl, index) => {
+      return (
+        <img
+          className={`letter-${index}`}
+          src={imgUrl}
+          ref={(img) => (imageElements[index] = img)}
+          alt="letter"
+        />
+      );
+    });
+  };
+
+  const randomPositionGenerator = (absoluteRange = 300) => {
+    let min = Math.ceil(-absoluteRange);
+    let max = Math.floor(absoluteRange);
+    const randomNumber = () =>
+      Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return {
+      x: randomNumber(),
+      y: randomNumber(),
+      z: randomNumber(),
+      autoAlpha: 0,
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>{renderLetters()}</div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/constants/images.js
@@ -0,0 +1,7 @@
+export const letterArray = [
+  "https://d3n32ilufxuvd1.cloudfront.net/5cefbdb4f3a58214725eee83/1449781/upload-82539eab-5f62-45c3-a5a1-3a4241dffe84.png",
+  "https://d3n32ilufxuvd1.cloudfront.net/5cefbdb4f3a58214725eee83/1449781/upload-882c24d4-5342-43a0-a8b9-3a34df370aea.png",
+  "https://d3n32ilufxuvd1.cloudfront.net/5cefbdb4f3a58214725eee83/1449781/upload-1bf1a262-1d74-46fa-8e76-b8069526dfc4.png",
+  "https://d3n32ilufxuvd1.cloudfront.net/5cefbdb4f3a58214725eee83/1449781/upload-2e278754-725e-4ab2-a526-1f1ee4ef5501.png",
+  "https://d3n32ilufxuvd1.cloudfront.net/5cefbdb4f3a58214725eee83/1449781/upload-0d146605-b23b-4600-813b-97289a1ff346.png",
+];
