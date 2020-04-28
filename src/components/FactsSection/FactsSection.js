1816e7ae8d5c597e97e5e4cf31689292b72b82f4
Stephanie Fashagba <stephanie.fashagba@thoughtworks.com>
2020-04-28 11:24:37 +0100

    Adds Facts Section with clipped text

---
+++ src/components/FactsSection/FactsSection.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Facts.css";
+
+export const FactsSection = () => {
+  return (
+    <div className="facts-section-container">
+      <h1 className="main-text">Deminology</h1>
+    </div>
+  );
+};
